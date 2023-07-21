efc6d5e5ed8d6b3d5665a1cf5f3b78414362c3d7
mohitr Agarwal <bmohit700@gmail.com>
2023-07-21 18:50:15 +0530

    implemented props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,12 @@
 import './ExpenseItem.css'
-function ExpenseItem() {
-  const expensedate=new Date(2023,6,22);
-  console.log(expensedate)
-  const expensetitle="Food"
-  const expenseAmount=100
-
+function ExpenseItem(props) {
   return (
     <div>
     <div className='expense-item'>
-      <div>{expensedate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{expensetitle}</h2>
-        <div className='expense-item__price'>Rs.{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>Rs.{props.price}</div>
         </div>
     </div>
     
