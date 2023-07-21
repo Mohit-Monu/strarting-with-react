2d781182eb678f9859cbbf78345afcb92ea81927
mohitr Agarwal <bmohit700@gmail.com>
2023-07-21 11:49:41 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,22 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Food</h2>
+        <div>10</div>
+      </div>
+      <div>Date</div>
+      <div>
+        <h2>Petrol</h2>
+        <div>100</div>
+      </div>
+      <div>Date</div>
+      <div>
+        <h2>Movies</h2>
+        <div>200</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem
