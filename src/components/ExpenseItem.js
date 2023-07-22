0405829292946897f470d844185ff2dd018f2c65
mohitr Agarwal <bmohit700@gmail.com>
2023-07-22 10:42:48 +0530

    implemented card

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
+import Card from "./Card"
 import "./ExpenseItem.css";
 function ExpenseItem(props) {
   return (
-    <div>
-      <div className="expense-item">
+
+      <Card className="expense-item">
         <ExpenseDate date={props.date}></ExpenseDate>
         <ExpenseDetails title={props.title} price={props.title}></ExpenseDetails>
-      </div>
-    </div>
+      </Card>
   );
 }
 export default ExpenseItem;
