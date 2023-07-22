16aca75c4875c2965b382bcd93f10edfc9cf785d
mohitr Agarwal <bmohit700@gmail.com>
2023-07-22 11:03:50 +0530

    some file structure

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,25 @@
+// import react from "react";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
+  // return react.createElement(
+  //   "div",
+  //   { className: "expense-item" },
+  //   react.createElement(ExpenseDate, { date: props.date }),
+  //   react.createElement(
+  //     "div",
+  //     {},
+  //     react.createElement("h1", {}, "Expense 1"),
+  //     react.createElement("h1", {}, "Expense 1")
+  //   )
+  // );
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <ExpenseDetails title={props.title} price={props.title}></ExpenseDetails>
+    </Card>
+  );
+}
+export default ExpenseItem;
