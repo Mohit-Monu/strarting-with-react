82c28ff699c6c5f97d6f0b0e60d2c6ba72ef1d11
mohitr Agarwal <bmohit700@gmail.com>
2023-07-25 10:18:22 +0530

    add eventlister

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -15,10 +15,18 @@ const ExpenseItem=(props)=> {
   //     react.createElement("h1", {}, "Expense 1")
   //   )
   // );
+  function editHandler(){
+    console.log("Edit!!")
+  }
+  function deleteHandler(){
+    console.log("Delete!!")
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <ExpenseDetails title={props.title} price={props.title}></ExpenseDetails>
+      <button onClick={editHandler} >Change Title</button>
+      <button onClick={deleteHandler} >Delete</button>
     </Card>
   );
 }
